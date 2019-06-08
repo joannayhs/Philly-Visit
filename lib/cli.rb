2483c491cc1c28f7ca777d1048304bd496ec668b
Joanna Yuhas <joannayuhas@gmail.com>
2019-06-08 15:21:32 -0400

    finished CLI logic

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,54 +2,58 @@ class CLI
 
   def call
     welcome
-    list_activities
-    goodbye
+    yes_or_no
   end
 
   def welcome
     puts "Welcome to Philadelphia, the City of Brotherly Love!"
   end
 
-  def list_activities
+  def yes_or_no
     puts "Is this your first time here? (Y/N)"
-    input = nil
-    while input != "exit"
-      input = gets.strip.upcase
-      if input == "Y"
-        puts "Here is a list of the ten most essential things to do on your first visit to Philly:"
-        puts "Which activity would you like to know more about? (1-10)"
-        number = nil
-        while number != "exit"
-        number = gets.strip
-        case number
-          when "1"
-            puts "first activity"
-          when "2"
-            puts "second activity"
-          when "3"
-            puts "third activity"
-          when "4"
-            puts "fourth activity"
-          when "5"
-            puts "fifth activity"
-          when "6"
-            puts "sixth activity"
-          when "7"
-            puts "seventh activity"
-          when "8"
-            puts "eighth activity"
-          when "9"
-            puts "ninth activity"
-          when "10"
-            puts "tenth activity"
-          else
-            puts "Please enter a number between 1 and 10."
-          end
-        end
-      elsif input == "N"
-        goodbye
-      end
+    input = gets.strip.upcase
+    if input == "Y"
+      activity_list
+    elsif input == "N"
+      goodbye
     end
+  end
+
+  def activity_list
+    puts "Here is a list of the ten most essential things to do on your first visit to Philly:"
+    puts "Which activity would you like to know more about? (1-10)"
+    puts "When finished, type 'exit'"
+    puts "list of activities"
+    number = nil
+    while number != "exit"
+      number = gets.strip
+      case number
+        when "1"
+          puts "first activity"
+        when "2"
+          puts "second activity"
+        when "3"
+          puts "third activity"
+        when "4"
+          puts "fourth activity"
+        when "5"
+          puts "fifth activity"
+        when "6"
+          puts "sixth activity"
+        when "7"
+          puts "seventh activity"
+        when "8"
+          puts "eighth activity"
+        when "9"
+          puts "ninth activity"
+        when "10"
+          puts "tenth activity"
+        when "exit"
+          goodbye
+        else
+          puts "Please enter a number 1-10:"
+        end
+      end
   end
 
   def goodbye
