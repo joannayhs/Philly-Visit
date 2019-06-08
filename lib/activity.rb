2d0fde69f2b5ce2212d5fc30d5b7ffc09f4ac0aa
Joanna Yuhas <joannayuhas@gmail.com>
2019-06-08 19:36:39 -0400

    Rethinking scraper and adding more information to activity class

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,7 +1,14 @@
 class Activity
-attr_accessor :overview, :location_and_contact, :why_visit, :must_see, :admission_info, :url
+attr_accessor :title, :overview, :location_and_contact, :admission_info, :url
+@@all = []
 
-#each instance of the activity will contain information about the specific acivity
-#
+def initialize(title = nil, overview = nil, location_and_contact = nil, admission_info = nil, url = nil)
+  @title, @overview, @location_and_contact, @admission_info, @url = title, overview, location_and_contact, admission_info, url
+  @@all << self
+end
+
+def self.all
+  @@all
+end
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,24 +3,39 @@ require "open-uri"
 require "nokogiri"
 
 class Scraper
-  attr_accessor :links, :activity
+
   BASE_URL = "https://www.visitphilly.com/articles/philadelphia/most-essential-things-to-do-in-philadelphia/"
 
-  def self.activity
-    page = open(BASE_URL)
-    doc = Nokogiri::HTML(page)
-    section_headings = doc.css("h2.vp-article-section__heading a")
-    @activity = section_headings.map{|activity| activity.text.strip}
-    @links = section_headings.map{|link| link.attr("href")}
+  def get_page
+    Nokogiri::HTML(open(BASE_URL))
   end
 
-  def self.scrape_info_page
-    attractions = {}
-    @links.each do |link|
-    page = open(link)
-    doc = Nokogiri::HTML(page)
-    binding.pry
+  def scrape_main_page
+    self.get_page.css("h2.vp-article-section__heading a")
+  end
+
+  def create_activity
+    scrape_main_page.each do |activity|
+      Activity.new(activity)
     end
-
   end
+
 end
+
+
+# def self.activity
+#   page = open(BASE_URL)
+#   doc = Nokogiri::HTML(page)
+#   section_headings = doc.css("h2.vp-article-section__heading a")
+#   activity = section_headings.map{|activity| activity.text.strip}
+#   @links = section_headings.map{|link| link.attr("href")}
+# end
+
+# def self.scrape_info_page
+#   attractions = {}
+#   @links.each do |link|
+#   page = open(link)
+#   doc = Nokogiri::HTML(page)
+#   binding.pry
+#   end
+# end
