fdb3d667a019c28d0f9e79b4f6b41f7fef6f0c10
Joanna Yuhas <joannayuhas@gmail.com>
2019-06-08 21:16:30 -0400

    created methods to parse the attraction name and url in Scraper class

--- lib/activity.rb
+++ lib/activity.rb
@@ -8,4 +8,5 @@ class Activity
 #it keeps track of all the attractions
 
 
+
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,19 +6,21 @@ class Scraper
 
   BASE_URL = "https://www.tripadvisor.com/Attractions-g60795-Activities-Philadelphia_Pennsylvania.html"
 
+
+
     def self.parse
       doc = Nokogiri::HTML(open(BASE_URL))
-      list_items = doc.css("div.attractions-attraction-overview-pois-PoiInfo__info--239IR>div div:nth-child(2) a.attractions-attraction-overview-pois-PoiInfo__name--SJ0a4").map{|attractions| attractions.text}
+    end
+
+    def self.attractions #gets the surface level of each activity to create the output list.
+      list_items = Scraper.parse.css("div.attractions-attraction-overview-pois-PoiInfo__info--239IR>div div:nth-child(2) a.attractions-attraction-overview-pois-PoiInfo__name--SJ0a4").map{|attractions| attractions.text}
       #creates an array of attractions
     end
 
-    def self.activity #gets the surface level of each activity to create the output list.
-      parse.map{|activity| activity.text.strip} #creates an array with the section headings from the website. They will be the titles of the Activity class.
+    def self.urls
+      list_items = Scraper.parse.css("div.attractions-attraction-overview-pois-PoiInfo__info--239IR>div div:nth-child(2) a.attractions-attraction-overview-pois-PoiInfo__name--SJ0a4").map{|attractions| attractions.attr('href')}
+      #creates an array of attraction urls to parse
     end
-
-    # def self.urls'
-    #   parse.map{|link| link.attr("href")} #creates an array of urls for each activity. These become the links used in the second level of scraping.
-    # end
 
     def self.parse_attraction_pages #scrapes the second level. pulls the information about each activity.
       attractions = {}
@@ -30,7 +32,5 @@ class Scraper
 
 end
 
-#rel="noopener noreferrer"
-# div.attractions-attraction-overview-pois-PoiInfo__info--239IR>div div:nth-child(2)
 
-Scraper.parse
+Scraper.urls
